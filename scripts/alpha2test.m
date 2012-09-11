d8d84fed83c475c7bced671954104e025174b0c4
rbaravalle <rbaravalle@gmail.com>
2012-09-11 14:51:48 -0300

    optimizations

--- scripts/alpha2test.m
+++ scripts/alpha2test.m
@@ -16,35 +16,32 @@ function res = alpha2test(ss,cuantas)
     
     l = 4;
     
-    measure = zeros(1,l,1);
+    measure = zeros(Ny,l);
     
     %C = cuantas; % how many alphas
 
     temp = log(1:2:(2*l));
-    
-   
+       
     for i = 1:Nx
         for j = 1:Ny % texel                            
-                %if(measure(k) == 0) measure(k) == eps; end
-                %v = img(i,j);
-            measure(1) = max(max(img(max(i-1,1):min(i+1,Nx),max(j-1,1):min(j+1,Ny))));
-            measure(2) = max(max(img(max(i-2,1):min(i+2,Nx),max(j-2,1):min(j+2,Ny))));
-            measure(3) = max(max(img(max(i-3,1):min(i+3,Nx),max(j-3,1):min(j+3,Ny))));
-            measure(4) = max(max(img(max(i-4,1):min(i+4,Nx),max(j-4,1):min(j+4,Ny))));
-            %measure(5) = max(max(img(max(i-5,1):min(i+5,Nx),max(j-5,1):min(j+5,Ny))));
-            
-            %if(measure(1) == 0) measure(1) = eps; end
-            %if(measure(2) == 0) measure(2) = eps; end
-            %if(measure(3) == 0) measure(3) = eps; end
-            %if(measure(4) == 0) measure(4) = eps; end
-            
-            % calculo de alfa: pendiente de la recta de ajuste
-            p2=[temp;ones(1,l)]'\log(measure+1)';
-            %p2 = polyfit(temp,log(measure),1); % 1: grado uno del polinomio
-            img2(i,j) = p2(1,:);
+            measure(j,1,i) = max(max(img(max(i-1,1):min(i+1,Nx),max(j-1,1):min(j+1,Ny))));
+            measure(j,2,i) = max(max(img(max(i-2,1):min(i+2,Nx),max(j-2,1):min(j+2,Ny))));
+            measure(j,3,i) = max(max(img(max(i-3,1):min(i+3,Nx),max(j-3,1):min(j+3,Ny))));
+            measure(j,4,i) = max(max(img(max(i-4,1):min(i+4,Nx),max(j-4,1):min(j+4,Ny))));
         end        
     end
-    %figure,imtool(img2);
+    toc;tic;
+    for i = 1:Nx       
+        AA=kron(speye(Ny), [temp;ones(1,l)]');
+        yvalues = log(measure(:,:,i)+1)';
+
+        bb=yvalues; bb=bb(:);
+        z=AA\bb;
+        z=reshape(z,2,[]);
+        img2(i,:)=z(1,:)'; 
+    end
+    toc;
+    tic;
     
     %%
     maxx = max(img2(:));
@@ -52,55 +49,11 @@ function res = alpha2test(ss,cuantas)
     
     paso = (maxx-minn)/cuantas;
     clases = (minn:paso:maxx-paso);
-    toc;
           
-    %tic;
-    % amount of texels between alpha and the next alpha
-    %function cant = contar(block,c) 
-        %cant = 0;
-        %alpha = clases(c);
-        %alpha1 = alpha;
-        %if(c ~= cuantas)
-        %    alpha1 = clases(c+1);
-        %else
-        %    alpha1 = alpha+1; % any number greater than alpha
-        %end
-        
-        %s1 = size(block,1);
-        %s2 = size(block,2);
-        
-       %cant = ((c == cuantas) && (sum(sum(block == clases(c+1))) > 0)) || (sum(sum((block >=  clases(c))))+sum(sum((block <  clases(c)+1))) == 2);
-        
-        %if(c ~= cuantas)
-            %for w = 1:s1,
-            %    for t = 1:s2,
-            %        b = block(w,t);
-            %        if (b >= alpha && b < alpha1)
-            %            cant = 1;
-            %        end
-            %        if(cant == 1) break; end
-            %    end
-            %    if(cant == 1) break; end
-            %end
-        %else
-            %for w = 1:s1,
-            %    for t = 1:s2,
-            %        if(block(w,t) == alpha1)
-            %            cant = 1;
-            %        end
-            %        if(cant == 1) break; end
-            %    end
-            %if(cant == 1) break; end
-            %end
-        %end
-    %end
-
     falpha = zeros(1,cuantas);
     cant = floor(log(Nx));
 
-    %res = [];
-    
-    tic;
+    %% falpha imagen
         
     img2 = [img2 img2; img2 img2];
     for c = 1:cuantas % one fractal dimension for each c
@@ -117,20 +70,12 @@ function res = alpha2test(ss,cuantas)
             for i = 1:numBlocks_x
                 for j = 1:numBlocks_y
                     
-                    block = img2((i-1)*sizeBlocks + 1:i*sizeBlocks, (j-1)*sizeBlocks + 1:j*sizeBlocks);
+                   block = img2((i-1)*sizeBlocks + 1:i*sizeBlocks, (j-1)*sizeBlocks + 1:j*sizeBlocks);
                     
-                    f = 0;
-                    %alpha = clases(c);
-                    %alpha1 = alpha;
-                    %if(c ~= cuantas)
-                    %    alpha1 = clases(c+1);
-                    %else
-                    %    alpha1 = alpha+1; % any number greater than alpha
-                    %end
-                    s1 = size(block,1);
-                    s2 = size(block,2);
+                   f = 0;
+                   s1 = size(block,1);
+                   s2 = size(block,2);
 
-                    %cant = ((c == cuantas) && (sum(sum(block == clases(c+1))) > 0)) || (sum(sum((block >=  clases(c))))+sum(sum((block <  clases(c)+1))) == 2);
                     if(c ~= cuantas)
                         for w = 1:s1,
                             for t = 1:s2,
@@ -158,12 +103,13 @@ function res = alpha2test(ss,cuantas)
                     %if(c == cuantas)
                     %    flag(i,j) = (sum(sum(block == clases(c)+1)) > 0);
                     %else
-                    %    flag(i,j) = (sum(sum((block >=  clases(c))))+sum(sum((block <  clases(c+1)))) == 2);
+                    %    flag(i,j) = (sum(sum((block >=  clases(c)))) > 0) && (sum(sum((block <  clases(c+1)))) > 0);
                     %end
+                    N(k+1) = N(k+1) + f; %flag(i,j);
                     %flag(i,j) = contar(img2((i-1)*sizeBlocks + 1:i*sizeBlocks, (j-1)*sizeBlocks + 1:j*sizeBlocks),c); %mark this if ANY part of block is true
                 end
             end
-            N(k+1)    = nnz(flag);
+            %N(k+1)    = nnz(flag);
             %toc;
         end
 
