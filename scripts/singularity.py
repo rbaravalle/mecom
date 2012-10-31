c830ba7bc1dd2b6fbd31e3cb84e11cbf89df43cf
rbaravalle <rbaravalle@gmail.com>
2012-10-31 13:36:33 -0300

    sigularity spectrum

--- scripts/singularity.py
+++ scripts/singularity.py
@@ -54,27 +54,31 @@ def spec(filename,v,b,cuantas):
 
     for i in range(0,Nx-1):
         for j in range(0,Ny-1):
-            measure[0] = min(gray.crop((max(i-1,0),max(j-1,0),min(i+1,Nx-1),min(j+1,Ny-1))).getdata()) + 1
-            measure[1] = min(gray.crop((max(i-2,0),max(j-2,0),min(i+2,Nx-1),min(j+2,Ny-1))).getdata()) + 1
-            measure[2] = min(gray.crop((max(i-3,0),max(j-3,0),min(i+3,Nx-1),min(j+3,Ny-1))).getdata()) + 1
-            measure[3] = min(gray.crop((max(i-4,0),max(j-4,0),min(i+4,Nx-1),min(j+4,Ny-1))).getdata()) + 1
-            #alphaIm[j,i] = np.polyfit(temp,np.log(measure),1)[0]
+            measure[0] = max(gray.crop((max(i-1,0),max(j-1,0),min(i+1,Nx-1),min(j+1,Ny-1))).getdata()) + 1
+            measure[1] = max(gray.crop((max(i-2,0),max(j-2,0),min(i+2,Nx-1),min(j+2,Ny-1))).getdata()) + 1
+            measure[2] = max(gray.crop((max(i-3,0),max(j-3,0),min(i+3,Nx-1),min(j+3,Ny-1))).getdata()) + 1
+            measure[3] = max(gray.crop((max(i-4,0),max(j-4,0),min(i+4,Nx-1),min(j+4,Ny-1))).getdata()) + 1
+            alphaIm[j,i] = np.polyfit(temp,np.log(measure),1)[0]
 
-    #maxim = np.max(alphaIm)
-    #minim = np.min(alphaIm)
+    maxim = np.max(alphaIm)
+    minim = np.min(alphaIm)
 
     # Alpha image
     #plt.imshow(alphaIm, cmap=matplotlib.cm.gray)
     #plt.show()
 
-    #paso = (maxim-minim)/cuantas;
-    #clases = np.arange(minim,maxim,paso);
-    #print "Alpha im", alphaIm
-    #print "Paso: ", paso
-    #print "Clases: ", clases
+    paso = (maxim-minim)/cuantas
+    clases = np.arange(minim,maxim,paso)
+    print "Clases: ", clases, cuantas
 
     # Window
-    cant = int(np.floor(np.log(Nx)));
+    cant = int(np.floor(np.log(Nx)))
+
+    hs = np.hstack((alphaIm,alphaIm))
+    alphaIm = np.vstack((hs,hs))
+
+    # Multifractal dimentions
+    falpha = np.zeros(cuantas)
 
     for c in range(cuantas):
         N = np.zeros(cant+1)
@@ -82,16 +86,53 @@ def spec(filename,v,b,cuantas):
             sizeBlocks = 2*k+1
             numBlocks_x = int(np.ceil(Nx/sizeBlocks))
             numBlocks_y = int(np.ceil(Ny/sizeBlocks))
-            print sizeBlocks, numBlocks_x, numBlocks_y
 
             flag = np.zeros((numBlocks_x,numBlocks_y))
 
             for i in range(1,numBlocks_x):
                 for j in range(1,numBlocks_y):
-                    block = alphaIm[(i-1)*sizeBlocks + 1:i*sizeBlocks, (j-1)*sizeBlocks + 1:j*sizeBlocks]
+                    xi = (i-1)*sizeBlocks
+                    xf = i*sizeBlocks-1
+                    yi = (j-1)*sizeBlocks
+                    yf = j*sizeBlocks-1
+                    if(xf == xi): xf = xf+1
+                    if(yf == yi): yf = yf+1
+                    block = alphaIm[xi : xf, yi : yf]
+
+                    f = 0;
+                    s1 = len(block)
+                    s2 = len(block[0])
+                    if(c != cuantas-1):
+                        for w in range(s1):
+                            for t in range(s2):
+                                b = block[w,t]
+                                if (b >= clases[c] and b < clases[c+1]):
+                                   f = 1
+                                if(f == 1):
+                                    break
+                            if(f == 1):
+                                break
+                    else:
+                        for w in range(s1):
+                            for t in range(s2):
+                                b = block[w,t]
+                                if (b == clases[c]): # !!
+                                   f = 1
+                                if(f == 1):
+                                    break
+                            if(f == 1):
+                                break
+                    
+                    flag[i-1,j-1] = f
+                    N[k] = N[k] + f;
+
+        falpha[c] = -np.polyfit(map(lambda i: np.log(i*2+1),range(cant+1)),np.log(map(lambda i: i+1,N)),1)[0]
 
     t =  time.clock()-t
-    print "Time: ", t
+    s = np.hstack((clases,falpha))
+    plot(s)
+    print "Time: ", t, "Res: ", falpha
+    return s
 
     
 
